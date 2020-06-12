f94445257d746a72e8a8daf97a8f55f5c4d1eada
AkaiTobira <techmaturgia.data@student.uj.edu.pl>
2020-06-12 11:55:26 +0200

    Tool Update

---
+++ Assets/Scripts/PlayerControllers/Utils/CommonValues.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class  CommonValues
+{
+    public static Vector2 PlayerVelocity = new Vector2();
+}
