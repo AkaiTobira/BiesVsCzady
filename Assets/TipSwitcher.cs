30d540af402690c1cbec1023b12db363949ad089
AkaiTobira <techmaturgia.data@student.uj.edu.pl>
2020-08-18 23:19:29 +0200

    Update

---
+++ Assets/TipSwitcher.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TipSwitcher : MonoBehaviour
+{
+
+    bool isVisible = false;
+
+    void Update()
+    {
+        if( GlobalUtils.PlayerObject.GetComponent<Player>().GetCurrentFormName().Contains("Cat") ){
+            isVisible = true;
+        }else{ 
+            isVisible = false;
+        }
+        
+
+        for( int i = 0; i< transform.childCount; i++){
+            transform.GetChild(i).gameObject.SetActive( isVisible );
+        }
+    }
+}
