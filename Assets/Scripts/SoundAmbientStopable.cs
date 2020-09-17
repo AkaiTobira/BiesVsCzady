0d9a544abef4cf8c3aa11c3aec027609cd7fd692
Katarzyna Płaczkowska <kat.placz@gmail.com>
2020-09-17 22:40:28 +0200

    new sfx, sound ambient stopable script

---
+++ Assets/Scripts/SoundAmbientStopable.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundAmbientStopable : SoundAmbient
+{
+
+    private void Start()
+    {
+        
+    }
+    public void PlaySFX(int soundId)
+    {
+        var sound = FMODUnity.RuntimeManager.CreateInstance(Sounds[soundId]);
+        FMODUnity.RuntimeManager.AttachInstanceToGameObject(sound, GetComponent<Transform>(), GetComponent<Rigidbody2D>());
+        sound.start();
+        //soundevents.Add(sound);
+    }
+}
